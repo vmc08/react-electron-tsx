cfae6dac953c1b41f1b4b52f488855aff5fcd0a1
Vivo Milo C. Crame <vmc08@yahoo.com>
2019-06-09 22:33:24 +0800

    update(app): fix infinite redirect memory leak

--- src/HOC/AccountVerifier.tsx
+++ src/HOC/AccountVerifier.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import UserContext from '../contexts/UserContext';
 
+const verificationPath = '/register/verify-email';
+const quizPath = '/register/quiz';
+
 export default <P extends object>(
   ComposedComponent: React.ComponentType<P>,
   blockAuthenticatedUser: boolean = false,
@@ -10,15 +13,16 @@ export default <P extends object>(
     static contextType = UserContext;
     render() {
       const { account, token }: any = this.context;
+      const { pathname } = window.location;
       if (account && token) {
         const { scored, verified } = account;
-        if (!verified) {
-          return <Redirect to="/register/verify-email" />;
+        if (!verified && pathname !== verificationPath) {
+          return <Redirect to={verificationPath} />;
         }
-        if (!scored) {
-          return <Redirect to="/register/quiz" />;
+        if (!scored && pathname !== quizPath) {
+          return <Redirect to={quizPath} />;
         }
-        if (blockAuthenticatedUser) {
+        if (blockAuthenticatedUser && scored && verified) {
           return <Redirect to="/" />;
         }
       }
